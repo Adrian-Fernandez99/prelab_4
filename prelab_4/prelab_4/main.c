050c6fe0837be016a46f80cd2040828936fa199a
Adrian-Fernandez99 <fer23381@uvg.edu.gt>
2025-04-04 12:23:26 -0600

    comentarios

--- prelab_4/prelab_4/main.c
+++ prelab_4/prelab_4/main.c
@@ -10,31 +10,39 @@ Author : Adrián Fernández
 Descripción: contador de 8 bits
  */ 
 
-
+// Librerias
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+// Se definen variables
 volatile uint8_t contador = 0;
 
+// Prototipo de funciones
 void setup();
 void cont_8bits();
 
+// Main loop
 int main(void)
 {
+	// Se llama función para setear
    setup();
 	
+	// Se inicia el loop
     while (1) 
     {
+		// Se llama función para mostrar contador
 		cont_8bits();
     }
 }
 
+// Función para mostrar el contador
 void cont_8bits()
 {
-	PORTB = (PORTB & 0xF0) | (contador & 0x0F);
-	PORTC = (PORTC & 0xF0) | ((contador >> 4) & 0x0F);
+	PORTB = (PORTB & 0xF0) | (contador & 0x0F); // se muestra el purto B los primeros 4 bits
+	PORTC = (PORTC & 0xF0) | ((contador >> 4) & 0x0F); // se mueven los 4 bits menos significativos a la derecha y se muestran
 }
 
+// Función para setear
 void setup()
 {
 	cli();
@@ -56,13 +64,15 @@ void setup()
 	sei();
 }
 
+// Interrupción de pinchange
 ISR(PCINT0_vect)
 {
-	
+	// Si el pin está encendido en el pin 4 incrementa
 	if (!(PINB & (1 << PORTB4)))
 	{
 		contador++;
 	}
+	// Si el pin está encendido en el pin 5 decrementa
 	if (!(PINB & (1 << PORTB5)))
 	{
 		contador--;
