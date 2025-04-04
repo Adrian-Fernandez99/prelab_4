5b90678a0b741c9619d5172ac57a3e3c7db1f024
Adrian-Fernandez99 <fer23381@uvg.edu.gt>
2025-04-04 11:56:57 -0600

    Primer borrados
    No se que estoy haciendo la verdad

--- prelab_4/prelab_4/main.c
+++ prelab_4/prelab_4/main.c
@@ -14,21 +14,57 @@ Descripcion: contador de 8 bits
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+volatile uint8_t contador = 0;
+volatile uint8_t boton = 0;
+
+void setup();
+void cont_8bits();
+
+int main(void)
+{
+   setup();
+	
+    while (1) 
+    {
+		cont_8bits();
+    }
+}
+
+void cont_8bits()
+{
+	PORTB = (PORTB & 0xF0) | (contador & 0x0F);
+	PORTB = (PORTB & 0xF0) | (contador & 0xF0);
+}
+
 void setup()
 {
+	cli();
+	
 	DDRD = 0xFF;
 	PORTD = 0x00;
 	
 	DDRB &= ~((1 << PORTB4) | (1 << PORTB5));
-	//PORTC |= (1 << PC0) | (1 << PC1);
+	PORTB |= ((1 << PORTB4) | (1 << PORTB5));
+	
+	PCMSK0 |= ~((1 << PORTB4) | (1 << PORTB5));
+	PCICR |= (1 << PCIE0);
+	
+	DDRD = 0x00;
+	PORTD = 0xFF;
 
+	sei();
 }
 
-int main(void)
+ISR(PCINT0_vect)
 {
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
+	uint8_t boton = PINB & ((1 << PORTB4) | (1 << PORTB5));
+	
+	if (!(boton & (1 << PORTB4)))
+	{
+		contador++;
+	}
+	if (!(boton & (1 << PORTB5)))
+	{
+		contador--;
+	}
 }
-
